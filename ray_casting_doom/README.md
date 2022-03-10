48c2657ee8d9a0f92078074c7eb28a806d38efa0
sunbro1337 <kirill.tulyakov231@gmail.com>
2022-03-10 19:01:05 +0300

    finaly part 5 sprite animations

--- ray_casting_doom/README.md
+++ ray_casting_doom/README.md
@@ -2,6 +2,6 @@ https://www.youtube.com/watch?v=SmKBsArp2dI - Done
 https://www.youtube.com/watch?v=6FwR56UKlYU - Done
 https://www.youtube.com/watch?v=UUw1KrbYe78 - Done
 https://www.youtube.com/watch?v=MgTHkqMjVa4 - Done
-https://www.youtube.com/watch?v=5Yl3UJXzhwM - In progress
+https://www.youtube.com/watch?v=5Yl3UJXzhwM - Done
 https://www.youtube.com/watch?v=rRqcyFAFqpQ - To be done
 https://www.youtube.com/watch?v=Ym-7UEPICto - To be done
--- ray_casting_doom/sprite.py
+++ ray_casting_doom/sprite.py
@@ -1,31 +1,84 @@
 import pygame
 from settings import *
 from resources_system import collect_sprites
+from collections import deque
 
 
 class Sprites:
     def __init__(self):
-        self.sprite_types = collect_sprites(SpritesConfig.PATH)
+        # self.sprite_types = collect_sprites(SpritesConfig.PATH)
+        self.sprite_parameters = {
+            'barrel': {
+                'sprite': pygame.image.load('sprites/barrel/base/0.png'),
+                'viewing_angles': False,
+                'shift': 1.8,
+                'scale': 0.4,
+                'animation': deque(
+                    [pygame.image.load(f'sprites/barrel/anim/{i}.png').convert_alpha() for i in range(12)]
+                ),
+                'animation_dist': 800,
+                'animation_speed': 10,
+            },
+            'pin': {
+                'sprite': pygame.image.load('sprites/pin/base/0.png').convert_alpha(),
+                'viewing_angles': None,
+                'shift': 0.6,
+                'scale': 0.6,
+                'animation': deque([pygame.image.load(f'sprites/pin/anim/{i}.png').convert_alpha() for i in range(8)]),
+                'animation_dist': 800,
+                'animation_speed': 10,
+                'blocked': True,
+            },
+            'kakodemon': {
+                'sprite': [pygame.image.load(f'sprites/kakodemon/base/{i}.png').convert_alpha() for i in range(8)],
+                'viewing_angles': True,
+                'shift': -0.2,
+                'scale': 1.1,
+                'animation': deque(
+                    [pygame.image.load(f'sprites/kakodemon/anim/{i}.png').convert_alpha() for i in range(9)]),
+                'animation_dist': 150,
+                'animation_speed': 10,
+                'blocked': True,
+            },
+            'flame': {
+                'sprite': pygame.image.load('sprites/flame/base/0.png').convert_alpha(),
+                'viewing_angles': None,
+                'shift': 0.7,
+                'scale': 0.6,
+                'animation': deque(
+                    [pygame.image.load(f'sprites/flame/anim/{i}.png').convert_alpha() for i in range(16)]),
+                'animation_dist': 800,
+                'animation_speed': 5,
+                'blocked': None,
+            },
+        }
         self.list_of_objects = [
-            SpriteObject(self.sprite_types['barrel'][0], True, (7.1, 2.1), 1.8, 0.4),
-            SpriteObject(self.sprite_types['barrel'][0], True, (5.9, 2.1), 1.8, 0.4),
-            SpriteObject(self.sprite_types['pedestal'][0], True, (8.8, 2.6), 1.6, 0.5),
-            SpriteObject(self.sprite_types['pedestal'][0], True, (8.8, 5.6), 1.6, 0.5),
-            SpriteObject(self.sprite_types['kakodemon'], False, (7, 4), -0.2, 0.7),
-            SpriteObject(self.sprite_types['kakodemon'], False, (5, 4), -0.2, 0.7),
+            SpriteObject(self.sprite_parameters['barrel'], (7.1, 2.1)),
+            SpriteObject(self.sprite_parameters['barrel'], (5.9, 2.1)),
+            SpriteObject(self.sprite_parameters['pin'], (8.7, 2.5)),
+            SpriteObject(self.sprite_parameters['kakodemon'], (7, 4)),
+            SpriteObject(self.sprite_parameters['flame'], (8.6, 5.6))
+            #SpriteObject(self.sprite_types['pedestal'][0], True, (8.8, 2.6), 1.6, 0.5),
+            #SpriteObject(self.sprite_types['pedestal'][0], True, (8.8, 5.6), 1.6, 0.5),
+            #SpriteObject(self.sprite_types['kakodemon'], False, (7, 4), -0.2, 0.7),
+            #SpriteObject(self.sprite_types['kakodemon'], False, (5, 4), -0.2, 0.7),
         ]
 
 
 class SpriteObject:
-    def __init__(self, object, static, pos, shift, scale):
-        self.object = object
-        self.static = static
+    def __init__(self, parameters, pos):
+        self.object = parameters['sprite']
+        self.viewing_angles = parameters['viewing_angles']
+        self.shift = parameters['shift']
+        self.scale = parameters['scale']
+        self.animation = parameters['animation']
+        self.animation_dist = parameters['animation_dist']
+        self.animation_speed = parameters['animation_speed']
+        self.animation_count = 0
         self.pos = self.x, self.y = pos[0] * ScreenConfig.TILE, pos[1] * ScreenConfig.TILE
-        self.shift = shift
-        self.scale = scale
 
-        if not static:
-            self.sprite_angles = [frozenset(range(i, i +45)) for i in range(0, 360, 45)]
+        if self.viewing_angles:
+            self.sprite_angles = [frozenset(range(i, i + 45)) for i in range(0, 360, 45)]
             self.sprite_positions = {angle: pos for angle, pos in zip(self.sprite_angles, self.object)}
 
     def object_locate(self, player, walls=None):
@@ -52,8 +105,8 @@ class SpriteObject:
             proj_height = min(int(RayCastingConfig.PROJ_COEF / distance_to_sprite * self.scale), ScreenConfig.DOUBLE_HEIGHT)
             half_proj_height = proj_height // 2
             shift = half_proj_height * self.shift
-
-            if not self.static:
+            # choosing sprite for angle
+            if self.viewing_angles:
                 if theta < 0:
                     theta += SpritesConfig.DOUBLE_PI
                 theta = 360 - int(math.degrees(theta))
@@ -63,8 +116,19 @@ class SpriteObject:
                         self.object = self.sprite_positions[angles]
                         break
 
+            #sprite animation
+            sprite_object = self.object
+            if self.animation and distance_to_sprite < self.animation_dist:
+                sprite_object = self.animation[0]
+                if self.animation_count < self.animation_speed:
+                    self.animation_count += 1
+                else:
+                    self.animation.rotate()
+                    self.animation_count = 0
+
+            # sprite scale and pos
             sprite_pos = (current_ray * RayCastingConfig.SCALE - half_proj_height, ScreenConfig.HALF_HEIGHT - half_proj_height + shift)
-            sprite = pygame.transform.scale(self.object, (proj_height, proj_height))
+            sprite = pygame.transform.scale(sprite_object, (proj_height, proj_height))
             return (distance_to_sprite, sprite, sprite_pos)
         else:
             return (False,)
