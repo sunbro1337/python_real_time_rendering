40dc398c8a4dedf7e488e525579695c4486297b1
sunbro1337 <kirill.tulyakov231@gmail.com>
2022-03-17 18:26:35 +0300

    part6: numba tests

---
+++ ray_casting_doom/test_numba.py
@@ -0,0 +1,17 @@
+import datetime
+
+from numba import jit
+import numpy as np
+
+x = np.arange(100).reshape(10, 10)
+a = datetime.datetime.now()
+@jit(nopython=True) # Set "nopython" mode for best performance, equivalent to @njit
+def go_fast(a): # Function is compiled to machine code when called the first time
+    trace = 0.0
+    for i in range(a.shape[0]):   # Numba likes loops
+        trace += np.tanh(a[i, i]) # Numba likes NumPy functions
+    return a + trace              # Numba likes NumPy broadcasting
+
+print(go_fast(x))
+b = datetime.datetime.now()
+print(b - a)
